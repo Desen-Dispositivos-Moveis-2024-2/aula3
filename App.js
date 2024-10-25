dbfa05e5bda8ae69e9054abec7f03e90d3ecd914
Prof-Karan-Luciano <karan.silva@ifro.edu.br>
2024-10-25 18:40:25 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import ScrollViewExample from './ScrollViewExample';
-
-export default function App () {
-    return (
-        <View style={styles.container}>
-            <ScrollViewExample>
-
-
-            </ScrollViewExample>
-            <StatusBar style="auto" />
-        </View>
-    );
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
